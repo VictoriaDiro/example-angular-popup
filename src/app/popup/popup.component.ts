019d6bce32288b2f8cc9b7b2806cdbe2170e70ba
VictoriaDiro <victoriadiro01@gmail.com>
2020-09-28 20:48:27 +0200

    Fix background and add effects to open popup

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.scss']
+  styleUrls: ['./popup.component.scss'],
+  animations: [
+    trigger('dialog', [
+      transition('void => *', [
+        style({ transform: 'scale3d(.3, .3, .3)' }),
+        animate(200)
+      ]),
+      transition('* => void', [
+        animate(200, style({ transform: 'scale3d(.0, .0, .0)' }))
+      ])
+    ])
+  ]
 })
 export class PopupComponent implements OnInit {
 
